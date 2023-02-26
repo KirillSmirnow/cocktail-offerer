67fc18c2aba4576db47238e13565386e66c78170
Ruslan <murzin.rus@numdes.com>
2023-02-26 13:28:56 +0300

    Fix tests

--- core/src/test/java/cocktailofferer/service/image/command/ImageMetadataCommandServiceTest.java
+++ core/src/test/java/cocktailofferer/service/image/command/ImageMetadataCommandServiceTest.java
@@ -4,13 +4,12 @@ package cocktailofferer.service.image.command;
 import cocktailofferer.service.image.ImageMetadataRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(classes = ImageMetadataCommandServiceTestConfiguration.class)
 class ImageMetadataCommandServiceTest {
     @Autowired
--- core/src/test/java/cocktailofferer/service/image/query/ImageMetadataQueryServiceTest.java
+++ core/src/test/java/cocktailofferer/service/image/query/ImageMetadataQueryServiceTest.java
@@ -4,7 +4,6 @@ import cocktailofferer.service.image.ImageMetadata;
 import cocktailofferer.service.image.ImageMetadataRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -12,7 +11,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(classes = ImageMetadataQueryServiceTestConfiguration.class)
 public class ImageMetadataQueryServiceTest {
     @Autowired
